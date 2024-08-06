4836cdb11e43bcf7f95ed25d00d650d562248488
Ansh <159460386+HiGautamAG@users.noreply.github.com>
2024-08-06 19:49:03 +0530

    Update rec.py

--- rec.py
+++ rec.py
@@ -14,3 +14,31 @@ def show(i):
 show(10)
 
 
+#call stack is used in recursion to store the function call.
+
+#Recursion is a common mathematical and programming concept. 
+# It means that a function calls itself. 
+# This has the benefit of meaning that you can loop through data to reach a result.
+
+
+def show(n):
+    if(n==0):
+        return
+    print(n)
+    show(n-1)
+    print("End of the function")
+
+show(4)
+
+#fact in recursion  #factorial of a number
+def factorial(a):
+    if a==1:
+        return 1
+    else:
+        return a*factorial(a-1)
+    
+print(factorial(5))
+
+
+
+
